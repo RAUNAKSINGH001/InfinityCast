4b387c947a38d4be7f2dc0ee809ac1054c3ab7aa
Raunak Singh <raunakr386@gmail.com>
2025-09-10 01:42:51 +0530

    Implemented User register controller

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -1,5 +1,17 @@
 import {Router} from 'express';
 import {registerUser} from '../controllers/user.controller.js'
 const router = Router();
-router.route("/register").post(registerUser)
+import {upload} from "../middlewaares/multer.middleware.js"
+router.route("/register").post( 
+   upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+        maxCount:1
+            }
+    ]),
+    registerUser)
 export default router;
