82b10db5ca7fa44cb191837297f0e6d6e9229d8a
Raunak Singh <raunakr386@gmail.com>
2025-09-12 14:23:22 +0530

    Implemented change Password features

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -361,7 +361,7 @@ const getUserChannelProfile = asyncHandler(async(req,res)=>
         isSubscribed:
         {
           $cond:{
-            if:{$in: [req.user?_id , "$subscribers.subscriber"]},
+            if:{$in: [req.user?._id , "$subscribers.subscriber"]},
             then:true,
 else:false
           }
@@ -389,6 +389,57 @@ else:false
   }
   return res.status(200).json(new ApiResponse(200 , channel[0] , "User channel fetched successfully"))
 }) 
+
+
+const getWatchHistory = asyncHandler(async(req,res)=>
+{
+  const user = await User.aggregate([
+    {
+      $match:{
+        _id:new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup:{
+        from:"videos",
+        localField:"watchHistory",
+        foreignField:"_id",
+        as:"watchHistory",
+        pipeline:[
+          {
+            $lookup :
+            {
+              from:"users",
+              localField:"owner",
+              foreignField:"_id",
+              as:"owner",
+              pipeline:[
+{
+  $project: {
+    fullName:1,
+    username:1,
+    avatar:1
+  }
+}
+              ]
+            }
+          },
+
+          {
+            $addFields:{
+              owner:{
+                $first:"$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+  return res.status(200).json(new ApiResponse(200,user[0].watchHistory,
+    "Watch history fetched successfully"
+  )) 
+})
 export {
   registerUser,
   loginUser,
@@ -400,4 +451,5 @@ export {
   updateUserAvatar,
   updateUserCoverImage,
   getUserChannelProfile,
+  getWatchHistory,
 };
