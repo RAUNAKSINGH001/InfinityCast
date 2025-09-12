ad226add19a88182c5b618698b43bc1421b2989c
Raunak Singh <raunakr386@gmail.com>
2025-09-12 15:11:22 +0530

    Implemented like , tweet , playlist and comment model

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 import express from "express";
-const app = express();
 import cookieParser from "cookie-parser";
 import cors from "cors";
+const app = express();
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -18,7 +19,25 @@ app.use(express.static("public"));
 app.use(cookieParser());
 
 import userRouter from "./src/routes/user.routes.js";
+import healthcheckRouter from "./src/routes/healthcheck.routes.js";
+import tweetRouter from "./src/routes/tweet.routes.js/";
+import subscriptionRouter from "./src/routes/subscription.routes.js";
+import videoRouter from "./src/routes/video.routes.js";
+import commentRouter from "./src/routes/comment.routes.js";
+import likeRouter from "./src/routes/like.routes.js";
+import playlistRouter from "./src/routes/playlist.routes.js";
+import dashboardRouter from "./src/routes/dashboard.routes.js";
+
 app.use("/api/v1/users", userRouter);
 // http://localhost:8000/api/v1/users/register
+
+app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
--- server/src/utils/cloudinary.js
+++ server/src/utils/cloudinary.js
@@ -1,32 +1,26 @@
-import fs from 'fs'
+import fs from "fs";
 
-import { v2 as cloudinary } from 'cloudinary';
+import { v2 as cloudinary } from "cloudinary";
 
+// Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET, // Click 'View API Keys' above to copy your API secret
+});
 
-    // Configuration
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
     });
+    fs.unlinkSync(localFilePath);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath)
+    return null;
+  }
+};
 
-const uploadOnCloudinary = async (localFilePath) =>
-{
-    try
-    {
-if(!localFilePath) return null;
-const response = await cloudinary.uploader.upload(localFilePath , {
-    resource_type:"auto"
-    
-})
-fs.unlinkSync(localFilePath)
-return response;
-    }
-    catch(error)
-    {
-
-    }
-}
-
-
-export {uploadOnCloudinary};
+export { uploadOnCloudinary };
